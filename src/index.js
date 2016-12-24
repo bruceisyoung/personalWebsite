d437c6c35b752b73ffd4ba70b7b5850fc99b96a2
bruceisyoung <bruce.isyoung2016@gmail.com>
2016-12-24 00:11:33 -0600

    basic setting for app component

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// import App from './components/app';
+// import routes from './components/router';
+// Render the router instead of any component.
+  // `<Provider store={store}>
+  //   {routes}
+  // </Provider>`
+
+ReactDOM.render(<p>haha</p>, document.getElementById('app'));
