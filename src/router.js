8eca71d5168cdc9d4f90a23844324916789daf43
bruceisyoung <bruce.isyoung2016@gmail.com>
2016-12-24 23:47:42 -0600

    basic router setting

---
+++ src/router.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {IndexRoute, Router, Route, browserHistory} from 'react-router';
+import App from './app';
+import LoginPage from '../containers/loginPage';
+import GamePlay from '../containers/gamePlay';
+
+// basic setup of router.
+const routes = (
+  <Router history={ browserHistory }>
+    <Route path='/' component={ App }>
+      <IndexRoute component={ LoginPage } />
+      <Route path='gamePlay' component={ GamePlay } />
+    </Route>
+  </Router>
+);
+
+export default routes;
