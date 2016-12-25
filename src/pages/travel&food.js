ea9806b9e8c96cacb24cf4559ce02f5819fa707b
bruceisyoung <bruce.isyoung2016@gmail.com>
2016-12-24 23:49:37 -0600

    personal website pages basic setting

---
+++ src/pages/travel&food.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class TravelFood extends Component {
+  render() {
+    return (
+      <div>
+        <p>Travel & Food</p>
+      </div>
+    );
+  }
+};
