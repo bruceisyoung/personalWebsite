23f6666cb1a3221868103181fc2500b29ca97da0
bruceisyoung <bruce.isyoung2016@gmail.com>
2017-01-06 23:16:34 -0800

    correct className, class bug in education.js

--- src/pages/education.js
+++ src/pages/education.js
@@ -4,63 +4,63 @@ export default class Education extends Component {
   render() {
     return (
 			<article className='content about white-bg' id='chaptereducation'>
-				<div class='inner'>
+				<div className='inner'>
 					<h2>Education</h2>
-					<div class="title-divider"></div>
+					<div className="title-divider"></div>
 					<h3>Just My Education</h3>
 					<p>say something!!!!!!@#$%^&*()(*&^%$#$%^&*(</p>
-					<div class="education-con">
-            <div class="container-sub">
-              <div class="full-divider"></div>
-              <div class="row">
-                <div class="education-details">
-                  <div class="col-6 margin-bottom50 margin-top50">
-                    <div class="col-3 icon-block"><i class="fa fa-photo"></i></div>
-                    <div class="flot-left">
+					<div className="education-con">
+            <div className="container-sub">
+              <div className="full-divider"></div>
+              <div className="row">
+                <div className="education-details">
+                  <div className="col-6 margin-bottom50 margin-top50">
+                    <div className="col-3 icon-block"><i className="fa fa-photo"></i></div>
+                    <div className="flot-left">
                       <h5>Hack Reactor</h5>
                       <h4>#$%^&*</h4>
                       <span>2016</span> </div>
                   </div>
-                  <div class="col-6 margin-bottom50 margin-top50 no-margin-top"> Phasellus nec gravida purus. Aliquam ac enim vel ipsum consectetur vulputate. Duis quis feugiat neque. Pellentesque eleifend, nisi vel mattis vestibulum, est lacus pretium quam. </div>
+                  <div className="col-6 margin-bottom50 margin-top50 no-margin-top"> Phasellus nec gravida purus. Aliquam ac enim vel ipsum consectetur vulputate. Duis quis feugiat neque. Pellentesque eleifend, nisi vel mattis vestibulum, est lacus pretium quam. </div>
                 </div>
-                <div class="full-divider"></div>
-                <div class="education-details">
-                  <div class="col-6 margin-bottom50 margin-top50">
-                    <div class="col-3 icon-block"><i class="fa fa-laptop"></i></div>
-                    <div class="flot-left">
+                <div className="full-divider"></div>
+                <div className="education-details">
+                  <div className="col-6 margin-bottom50 margin-top50">
+                    <div className="col-3 icon-block"><i className="fa fa-laptop"></i></div>
+                    <div className="flot-left">
                       <h5>Fudan University</h5>
                       <h4>Master of Science</h4>
                       <span>Micro-Electronics</span><br />
                       <span>2008 - 2011</span>
                     </div>
                   </div>
-                  <div class="col-6 margin-bottom50 margin-top50 no-margin-top"> Phasellus nec gravida purus. Aliquam ac enim vel ipsum consectetur vulputate. Duis quis feugiat neque. Pellentesque eleifend, nisi vel mattis vestibulum, est lacus pretium quam. </div>
+                  <div className="col-6 margin-bottom50 margin-top50 no-margin-top"> Phasellus nec gravida purus. Aliquam ac enim vel ipsum consectetur vulputate. Duis quis feugiat neque. Pellentesque eleifend, nisi vel mattis vestibulum, est lacus pretium quam. </div>
                 </div>
-                <div class="full-divider"></div>
-                <div class="education-details">
-                  <div class="col-6 margin-bottom50 margin-top50">
-                    <div class="col-3 icon-block"><i class="fa fa-laptop"></i></div>
-                    <div class="flot-left">
+                <div className="full-divider"></div>
+                <div className="education-details">
+                  <div className="col-6 margin-bottom50 margin-top50">
+                    <div className="col-3 icon-block"><i className="fa fa-laptop"></i></div>
+                    <div className="flot-left">
                       <h5>National Chiao-Tung University</h5>
                       <h4>Exchange Student</h4>
                       <span></span><br />
                       <span>2010 - 2011</span>
                     </div>
                   </div>
-                  <div class="col-6 margin-bottom50 margin-top50 no-margin-top"> Phasellus nec gravida purus. Aliquam ac enim vel ipsum consectetur vulputate. Duis quis feugiat neque. Pellentesque eleifend, nisi vel mattis vestibulum, est lacus pretium quam. </div>
+                  <div className="col-6 margin-bottom50 margin-top50 no-margin-top"> Phasellus nec gravida purus. Aliquam ac enim vel ipsum consectetur vulputate. Duis quis feugiat neque. Pellentesque eleifend, nisi vel mattis vestibulum, est lacus pretium quam. </div>
                 </div>
-                <div class="full-divider"></div>
-                <div class="education-details">
-                  <div class="col-6 margin-bottom50 margin-top50">
-                    <div class="col-3 icon-block"><i class="fa fa-life-bouy"></i></div>
-                    <div class="flot-left">
+                <div className="full-divider"></div>
+                <div className="education-details">
+                  <div className="col-6 margin-bottom50 margin-top50">
+                    <div className="col-3 icon-block"><i className="fa fa-life-bouy"></i></div>
+                    <div className="flot-left">
                       <h5>Fudan University</h5>
                       <h4>Bachelor of Science</h4>
                       <span>Micro-Electronics</span> <br />
                       <span>2004 - 2005</span>
                     </div>
                   </div>
-                  <div class="col-6 margin-bottom50 margin-top50 no-margin-top"> Phasellus nec gravida purus. Aliquam ac enim vel ipsum consectetur vulputate. Duis quis feugiat neque. Pellentesque eleifend, nisi vel mattis vestibulum, est lacus pretium quam. </div>
+                  <div className="col-6 margin-bottom50 margin-top50 no-margin-top"> Phasellus nec gravida purus. Aliquam ac enim vel ipsum consectetur vulputate. Duis quis feugiat neque. Pellentesque eleifend, nisi vel mattis vestibulum, est lacus pretium quam. </div>
                 </div>
               </div>
             </div>
