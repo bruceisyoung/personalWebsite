d04a5c89702845a906aedccab29f3748b09cc713
bruceisyoung <bruce.isyoung2016@gmail.com>
2017-01-08 02:31:23 -0800

    fix the bugs of links not transfer correctly

--- src/components/app.js
+++ src/components/app.js
@@ -26,18 +26,14 @@ export default class App extends Component {
       		<a href='#menu1' className='menu-link'></a>
           {/* left navigation */}
       		<nav id='menu1' role='navigation'>
-      			<div className='jspContainer'>
-              <div className='jspPane'>
-                <a href='#chapterhome'><span id='link_home'>Home</span></a>
-                <a href='#chapterportfolio'><span id='link_portfolio'>Portfolio</span></a>
-                <a href='#chapterskills'><span id='link_skills'>Skills</span></a>
-                <a href='#chapterprojects'><span id='link_projects'>Projects</span></a>
-                <a href='#chapterexperience'><span id='link_experience'>Experience</span></a>
-                <a href='#chaptereducation'><span id='link_education'>Education</span></a>
-                <a href='#chaptertravelfood'><span id='link_travelfood'>Travel & Food</span></a>
-                <a href='#chaptercontact'><span id='link_contact'>Contact</span></a>
-              </div>
-	      		</div>
+            <a href='#chapterhome'><span id='link_home'>Home</span></a>
+            <a href='#chapterportfolio'><span id='link_portfolio'>Portfolio</span></a>
+            <a href='#chapterskills'><span id='link_skills'>Skills</span></a>
+            <a href='#chapterprojects'><span id='link_projects'>Projects</span></a>
+            <a href='#chapterexperience'><span id='link_experience'>Experience</span></a>
+            <a href='#chaptereducation'><span id='link_education'>Education</span></a>
+            <a href='#chaptertravelfood'><span id='link_travelfood'>Travel & Food</span></a>
+            <a href='#chaptercontact'><span id='link_contact'>Contact</span></a>
       		</nav>
       	</aside>
       	<div className='content-scroller'>
--- src/lib/jquery.page.js
+++ src/lib/jquery.page.js
@@ -1,285 +1,284 @@
-var pagetitle=$(document).find("title").text();
-
-(function(window, undefined) {
-			"use strict"; 	
-	var Page	= (function() {
+$(document).ready(function() {
 	
-		var $container			= $( '#container' ),
-			// the scroll container that wraps the articles
-			$scroller			= $container.find( 'div.content-scroller' ),
-			$menu				= $container.find( 'aside' ),
-			// menu links
-			$links				= $menu.find( 'nav > a' ),
-			$articles			= $container.find( 'div.content-wrapper > article' ).not(".noscroll"),
-			
-		
-			
-			// button to scroll to the top of the page
-			// only shown when screen size < 715
-			$toTop				= $container.find( 'a.totop-link' ),
-			// the browser nhistory object
-			History 			= window.History,
-			// animation options
-			animation			= { speed : 800, easing : 'easeInOutExpo' },
-			// jScrollPane options
-			scrollOptions		= { verticalGutter : 0, hideFocus : true },
-			// init function
-			init				= function() {
-				
-				// initialize the jScrollPane on both the menu and articles
-				_initCustomScroll();
-				// initialize some events
-				_initEvents();
-				// sets some css properties 
-				_layout();
-				// jumps to the respective chapter
-				// according to the url
-				_goto();
-				
-			},
-			_initCustomScroll	= function() {
-			
+	var pagetitle=$(document).find("title").text();
 
-				// Only add custom scroll to articles if screen size > 715.
-				// If not the articles will be expanded
-				if( $(window).width() > 767 ) {
-				
-					$articles.jScrollPane( scrollOptions );
-				
-				}
-				// add custom scroll to menu
-				$menu.children( 'nav' ).jScrollPane( scrollOptions );
-			
-			},
-			_goto				= function( chapter ) {
-				
-				
-				$(document).attr("title",pagetitle);
-				
-				
-					// get the url from history state (e.g. chapter=3) and extract the chapter number
-				var chapter 	= chapter || History.getState().url.queryStringToJSON().page,
-					isHome		= ( chapter === undefined ),
-					// we will jump to the introduction chapter if theres no chapter
-					$article 	= $( chapter ? '#' + 'chapter' + chapter : '#' + 'introduction' );
-				
-				
-				
-					
-					$('#link_home').removeClass('active');
-					$('#link_portfolio').removeClass('active');
-					$('#link_skills').removeClass('active');
-					$('#link_projects').removeClass('active');
-					$('#link_experience').removeClass('active');
-					$('#link_education').removeClass('active');
-					$('#link_travelfood').removeClass('active');
-					$('#link_contact').removeClass('active');
-					
-					$('#link_'+chapter).addClass('active');
-					
-				if( $article.length ) {
-				
-						// left / top of the element
-					var left		= $article.position().left,
-						top			= $article.position().top,
-						// check if we are scrolling down or left
-						// is_v will be true when the screen size < 715
-						is_v		= ( $(document).height() - $(window).height() > 0 ),
-						// animation parameters:
-						// if vertically scrolling then the body will animate the scrollTop,
-						// otherwise the scroller (div.content-scroller) will animate the scrollLeft
-						param		= ( is_v ) ? { scrollTop : (isHome) ? top : top + $menu.outerHeight( true ) } : { scrollLeft : left },
-						$elScroller	= ( is_v ) ? $( 'html, body' ) : $scroller;
-					
-					$elScroller.stop().animate( param, animation.speed, animation.easing, function() {
-						
-						// active class for selected chapter
-						///$articles.removeClass( 'content-active' );
-						//$article.addClass( 'content-active' );
-						
-						
-					} );
-				
-				}
-			
-			},
-			_saveState			= function( chapter ) {
-			
-				// adds a new state to the history object
-				// this will trigger the statechange on the window
-				if( History.getState().url.queryStringToJSON().page !== chapter ) {
-						
-					History.pushState( null, null, '?page=' + chapter );
+	(function(window, undefined) {
+		"use strict"; 	
+		var Page	= (function() {
 		
-					$('#link_home').removeClass('active');
-					$('#link_portfolio').removeClass('active');
-					$('#link_skills').removeClass('active');
-					$('#link_projects').removeClass('active');
-					$('#link_experience').removeClass('active');
-					$('#link_education').removeClass('active');
-					$('#link_travelfood').removeClass('active');
-					$('#link_contact').removeClass('active');
+			var $container			= $( '#container' ),
+				// the scroll container that wraps the articles
+				$scroller			= $container.find( 'div.content-scroller' ),
+				$menu				= $container.find( 'aside' ),
+				// menu links
+				$links				= $menu.find( 'nav > a' ),
+				$articles			= $container.find( 'div.content-wrapper > article' ).not(".noscroll"),
+				
+			
+				
+				// button to scroll to the top of the page
+				// only shown when screen size < 715
+				$toTop				= $container.find( 'a.totop-link' ),
+				// the browser nhistory object
+				History 			= window.History,
+				// animation options
+				animation			= { speed : 800, easing : 'easeInOutExpo' },
+				// jScrollPane options
+				scrollOptions		= { verticalGutter : 0, hideFocus : true },
+				// init function
+				init				= function() {
 					
-					$('#link_'+chapter).addClass('active');
+					// initialize the jScrollPane on both the menu and articles
+					_initCustomScroll();
+					// initialize some events
+					_initEvents();
+					// sets some css properties 
+					_layout();
+					// jumps to the respective chapter
+					// according to the url
+					_goto();
+					
+				},
+				_initCustomScroll	= function() {
 				
-				}
-			
-			},
-			_layout				= function() {
-			
-				// control the overflow property of the scroller (div.content-scroller)
-				var windowWidth	= $(window).width();
-				
-				var isipad = navigator.userAgent.toLowerCase().indexOf("ipad");
-				if(isipad > -1)
-				{
-					//$('.totop-link').hide();
-					switch( true ) {
-					case ( windowWidth <= 768 ) : $scroller.scrollLeft( 0 ).css( 'overflow', 'visible' ); break;
-					case ( windowWidth <= 1024 ): $scroller.css( 'overflow-x', 'hidden' ); break;
-					case ( windowWidth > 1024 ) : $scroller.css( 'overflow', 'hidden' ); break;
-					};
-				}
-				else
-				{
-					switch( true ) {
-					case ( windowWidth <= 768 ) : $scroller.scrollLeft( 0 ).css( 'overflow', 'visible' ); break;
-					case ( windowWidth <= 1024 ): $scroller.css( 'overflow-x', 'hidden' ); break;
-					case ( windowWidth > 1024 ) : $scroller.css( 'overflow', 'hidden' ); break;
-					};
-				}
-			
-			},
-			_initEvents			= function() {
-				
-				_initWindowEvents();
-				_initMenuEvents();
-				_initArticleEvents();
-				
-			},
-			_initWindowEvents	= function() {
-			
-				$(window).on({
 
-					// when resizing the window we need to reinitialize or destroy the jScrollPanes
-					// depending on the screen size
-					'smartresize' : function( event ) {
-						
-						var isipad = navigator.userAgent.toLowerCase().indexOf("ipad");
-						var ismobile =  navigator.userAgent.toLowerCase().indexOf("mobile");
-						if(isipad > -1 || ismobile > -1 )
-						{
-						}
-						else
-						{
-							_layout();
-							
-							$('article.content').not(".noscroll").each( function() {
-								
-								var $article 	= $(this),
-									aJSP		= $article.data( 'jsp' );
-								
-								if( $(window).width() > 767 ) {
-									
-									( aJSP === undefined ) ? $article.jScrollPane( scrollOptions ) : aJSP.reinitialise();
-									
-									_initArticleEvents();
-									
-								}	
-								else {
-									
-									// destroy article's custom scroll if screen size <= 715px
-									if( aJSP !== undefined )
-										aJSP.destroy();
-									
-									$container.off( 'click', 'article.content' );
-									
-								}
-								
-							});
-							
-							var nJSP = $menu.children( 'nav' ).data( 'jsp' );
-							nJSP.reinitialise();
-							
-							// jumps to the current chapter
-							_goto();
-						}
-					
-					},
-					// triggered when the history state changes - jumps to the respective chapter
-					'statechange' : function( event ) {
-						
-						_goto();
+					// Only add custom scroll to articles if screen size > 715.
+					// If not the articles will be expanded
+					if( $(window).width() > 767 ) {
+						$articles.jScrollPane();
 					
 					}
-				});
-			
-			},
-			_initMenuEvents		= function() {
-			
-				// when we click a menu link we check which chapter the link refers to,
-				// and we save the state on the history obj.
-				// the statechange of the window is then triggered and the page/scroller scrolls to the 
-				// respective chapter's position
-				$links.on( 'click', function( event ) {
+					// add custom scroll to menu
+					$menu.children( 'nav' ).jScrollPane();
+				
+				},
+				_goto				= function( chapter ) {
+					$(document).attr("title",pagetitle);
 					
-					var href		= $(this).attr('href'),
-						chapter		= ( href.search(/chapter/) !== -1 ) ? href.substring(8) : 0;
 					
-					_saveState( chapter );
-					if (href.indexOf("#") != -1) 
-						return false;
-					else 
-						return true;
-					
+						// get the url from history state (e.g. chapter=3) and extract the chapter number
+					var chapter 	= chapter || History.getState().url.queryStringToJSON().page,
+						isHome		= ( chapter === undefined ),
+						// we will jump to the introduction chapter if theres no chapter
+						$article 	= $( chapter ? '#' + 'chapter' + chapter : '#' + 'home' );
+
+						$('#link_home').removeClass('active');
+						$('#link_portfolio').removeClass('active');
+						$('#link_skills').removeClass('active');
+						$('#link_projects').removeClass('active');
+						$('#link_experience').removeClass('active');
+						$('#link_education').removeClass('active');
+						$('#link_travelfood').removeClass('active');
+						$('#link_contact').removeClass('active');
 						
+						$('#link_'+chapter).addClass('active');
+						
+					if( $article.length ) {
+					
+							// left / top of the element
+						var left		= $article.position().left,
+							top			= $article.position().top,
+							// check if we are scrolling down or left
+							// is_v will be true when the screen size < 715
+							is_v		= ( $(document).height() - $(window).height() > 0 ),
+							// animation parameters:
+							// if vertically scrolling then the body will animate the scrollTop,
+							// otherwise the scroller (div.content-scroller) will animate the scrollLeft
+							param		= ( is_v ) ? { scrollTop : (isHome) ? top : top + $menu.outerHeight( true ) } : { scrollLeft : left },
+							$elScroller	= ( is_v ) ? $( 'html, body' ) : $scroller;
+						
+						$elScroller.stop().animate( param, animation.speed, animation.easing, function() {
+							
+							// active class for selected chapter
+							///$articles.removeClass( 'content-active' );
+							//$article.addClass( 'content-active' );
+							
+							
+						} );
+					
+					}
 				
-				});
-			
-				// scrolls to the top of the page.
-				// this button will only be visible for screen size < 715
-				$toTop.on( 'click', function( event ) {
-					
-					$( 'html, body' ).stop().animate( { scrollTop : 0 }, animation.speed, animation.easing );
-					
-					return false;
+				},
+				_saveState			= function( chapter ) {
 				
-				});
-			
-			},
-			_initArticleEvents	= function() {
-			
-				// when we click on an article we check which chapter the article refers to,
-				// and we save the state on the history obj.
-				// the statechange of the window is then triggered and the page/scroller scrolls to the 
-				// respective chapter's position
+					// adds a new state to the history object
+					// this will trigger the statechange on the window
+					if( History.getState().url.queryStringToJSON().page !== chapter ) {
+							
+						History.pushState( null, null, '?page=' + chapter );
+
+						$('#link_home').removeClass('active');
+						$('#link_portfolio').removeClass('active');
+						$('#link_skills').removeClass('active');
+						$('#link_projects').removeClass('active');
+						$('#link_experience').removeClass('active');
+						$('#link_education').removeClass('active');
+						$('#link_travelfood').removeClass('active');
+						$('#link_contact').removeClass('active');
+						
+						$('#link_'+chapter).addClass('active');
+					
+					}
 				
+				},
+				_layout				= function() {
+				
+					// control the overflow property of the scroller (div.content-scroller)
+					var windowWidth	= $(window).width();
 					
-					
-					if($(window).width()>768)
+					var isipad = navigator.userAgent.toLowerCase().indexOf("ipad");
+					if(isipad > -1)
 					{
-						$container.on( 'click', 'article.content', function( event ) {
-							
-							var id			= $(this).attr('id'),
-								chapter		= ( id.search(/chapter/) !== -1 ) ? id.substring(7) : 0;
-							
-							_saveState( chapter );
-							
-							//return false;
-						
-						});
+						//$('.totop-link').hide();
+						switch( true ) {
+						case ( windowWidth <= 768 ) : $scroller.scrollLeft( 0 ).css( 'overflow', 'visible' ); break;
+						case ( windowWidth <= 1024 ): $scroller.css( 'overflow-x', 'hidden' ); break;
+						case ( windowWidth > 1024 ) : $scroller.css( 'overflow', 'hidden' ); break;
+						};
 					}
+					else
+					{
+						switch( true ) {
+						case ( windowWidth <= 768 ) : $scroller.scrollLeft( 0 ).css( 'overflow', 'visible' ); break;
+						case ( windowWidth <= 1024 ): $scroller.css( 'overflow-x', 'hidden' ); break;
+						case ( windowWidth > 1024 ) : $scroller.css( 'overflow', 'hidden' ); break;
+						};
+					}
+				
+				},
+				_initEvents			= function() {
 					
+					_initWindowEvents();
+					_initMenuEvents();
+					_initArticleEvents();
 					
+				},
+				_initWindowEvents	= function() {
+				
+					$(window).on({
+
+						// when resizing the window we need to reinitialize or destroy the jScrollPanes
+						// depending on the screen size
+						'smartresize' : function( event ) {
+							
+							var isipad = navigator.userAgent.toLowerCase().indexOf("ipad");
+							var ismobile =  navigator.userAgent.toLowerCase().indexOf("mobile");
+							if(isipad > -1 || ismobile > -1 )
+							{
+							}
+							else
+							{
+								_layout();
+								
+								$('article.content').not(".noscroll").each( function() {
+									
+									var $article 	= $(this),
+										aJSP		= $article.data( 'jsp' );
+									
+									if( $(window).width() > 767 ) {
+										
+										( aJSP === undefined ) ? $article.jScrollPane( scrollOptions ) : aJSP.reinitialise();
+										
+										_initArticleEvents();
+										
+									}	
+									else {
+										
+										// destroy article's custom scroll if screen size <= 715px
+										if( aJSP !== undefined )
+											aJSP.destroy();
+										
+										$container.off( 'click', 'article.content' );
+										
+									}
+									
+								});
+								
+								var nJSP = $menu.children( 'nav' ).data( 'jsp' );
+								(nJSP === undefined) ? $menu.children( 'nav' ).jScrollPane( scrollOptions ) : nJSP.reinitialise();
+								
+								// jumps to the current chapter
+								_goto();
+							}
+						
+						},
+						// triggered when the history state changes - jumps to the respective chapter
+						'statechange' : function( event ) {
+							
+							_goto();
+						
+						}
+					});
+				
+				},
+				_initMenuEvents		= function() {
+				
+					// when we click a menu link we check which chapter the link refers to,
+					// and we save the state on the history obj.
+					// the statechange of the window is then triggered and the page/scroller scrolls to the 
+					// respective chapter's position
+					console.log('links: ', $menu, $links);
+					$links.on( 'click', function( event ) {
+						console.log($links);
+						console.log(event.target, $(this));
+						var href		= $(this).attr('href'),
+							chapter		= ( href.search(/chapter/) !== -1 ) ? href.substring(8) : 0;
+						_saveState( chapter );
+						if (href.indexOf("#") != -1) 
+							return false;
+						else 
+							return true;
+						
+							
+					
+					});
+				
+					// scrolls to the top of the page.
+					// this button will only be visible for screen size < 715
+					$toTop.on( 'click', function( event ) {
+						
+						$( 'html, body' ).stop().animate( { scrollTop : 0 }, animation.speed, animation.easing );
+						
+						return false;
+					
+					});
+				
+				},
+				_initArticleEvents	= function() {
+				
+					// when we click on an article we check which chapter the article refers to,
+					// and we save the state on the history obj.
+					// the statechange of the window is then triggered and the page/scroller scrolls to the 
+					// respective chapter's position
+					
+						
+						
+						if($(window).width()>768)
+						{
+							$container.on( 'click', 'article.content', function( event ) {
+								
+								var id			= $(this).attr('id'),
+									chapter		= ( id.search(/chapter/) !== -1 ) ? id.substring(7) : 0;
+								
+								_saveState( chapter );
+								
+								//return false;
+							
+							});
+						}
+						
+						
+						
 					
 				
-			
-			};
-			
-		return { init : init };	
-	
-	})();
-	
-	Page.init();
-	
-})(window);
+				};
+				
+			return { init : init };	
+		
+		})();
+		
+		Page.init();
+		
+	})(window);
+});
+
