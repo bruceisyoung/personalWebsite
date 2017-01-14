b8ceabbd08cb43cb18d113e8b20274ef79efd5ae
bruceisyoung <bruce.isyoung2016@gmail.com>
2017-01-13 17:24:16 -0800

    add pics at the home page and delete the pic on the portfolio page

--- src/pages/home.js
+++ src/pages/home.js
@@ -12,6 +12,11 @@ export default class Home extends Component {
         	</h2>
         	<span className='title'>Full-Stack Software Engineer</span>
       	</div>
+        <div id="owl-demo" className="owl-carousel">
+          <div className="item"><img src="../src/assets/images/profile1.jpg" alt="" /></div>
+          <div className="item"><img src="../src/assets/images/profile2.jpg" alt="" /></div>
+          <div className="item"><img src="../src/assets/images/profile3.jpg" alt="" /></div>
+        </div>
       	<img/>
 	    </article>
 
