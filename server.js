4489eddd18723d89a8b341aa3c051baefda02ba2
bruceisyoung <bruce.isyoung2016@gmail.com>
2017-01-18 22:16:36 -0800

    add body-parser in the server for the server to get the correct information

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser');
 var port = process.env.PORT || 8080;
 var app = express();
 
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
 app.use(express.static(__dirname));
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'));
+});
+
+app.post('/message', (req, res) => {
+	console.log('somebody is posting');
+	console.log('reqxxxxxxxx: ', req);
+	console.log('bodyxxxxxxx: ',req.body);
+	console.log('dataxxxxxxx: ', req.data);
+	console.log('keysxxxxxxx', Object.keys(req));
 });
 
 app.listen(port);
