bcdb80d3bfc19a8aabc3437197dd21356554ef2c
bruceisyoung <bruce.isyoung2016@gmail.com>
2017-01-20 13:41:54 -0600

    reset webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+const config = {
   entry: [
     './src/index.js'
   ],
@@ -19,8 +19,41 @@ module.exports = {
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+          'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
+  ],
   devServer: {
     historyApiFallback: true,
     contentBase: './'
   }
 };
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        screw_ie8: true
+      }
+    })
+  );
+  babelSettings.plugins.push("transform-react-inline-elements");
+  babelSettings.plugins.push("transform-react-constant-elements");
+} else {
+  config.devtool = "#cheap-module-source-map"
+  config.devServer = {
+    contentBase: './public',
+    hot: true,
+    inline: true,
+    host: "0.0.0.0",
+    port: 2708
+  }
+  config.plugins.push(
+    new webpack.HotModuleReplacementPlugin()
+  );
+}
+
+module.exports = config;
